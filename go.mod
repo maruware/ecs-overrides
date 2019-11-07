24a49486e3627f1cf084536b4ac071c84f0f18ae
maruware <me@maruware.com>
2019-11-07 11:56:30 +0900

    abolish sub command

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.12
 
 require (
 	github.com/aws/aws-sdk-go v1.25.27
-	github.com/mattn/go-shellwords v1.0.6
+	github.com/stretchr/testify v1.4.0 // indirect
 	github.com/urfave/cli v1.22.1
+	golang.org/x/net v0.0.0-20191105084925-a882066a44e0 // indirect
 )
--- main.go
+++ main.go
@@ -1,20 +1,44 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"log"
 	"os"
 
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/ecs"
 	"github.com/urfave/cli"
 )
 
-var Version = "0.0.1"
+var Version = "0.0.2"
 
 func main() {
 	app := cli.NewApp()
 	app.Version = Version
+	app.Usage = "[name] [command...]"
+	app.Action = func(c *cli.Context) error {
+		if c.NArg() < 2 {
+			return fmt.Errorf("shortage args")
+		}
+		name := c.Args().Get(0)
+		command := c.Args()[1:]
 
-	app.Commands = []cli.Command{
-		cmdCommand(),
+		co := ecs.ContainerOverride{
+			Name:    aws.String(name),
+			Command: aws.StringSlice(command),
+		}
+		cos := []*ecs.ContainerOverride{&co}
+		ov := ecs.TaskOverride{
+			ContainerOverrides: cos,
+		}
+		j, err := json.Marshal(ov)
+		if err != nil {
+			return err
+		}
+		fmt.Print(string(j))
+
+		return nil
 	}
 
 	err := app.Run(os.Args)
